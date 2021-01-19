bf479a116f7000fd87e84c1e30f8f65527889bb9
RTeam1 <77674437+RTeam1@users.noreply.github.com>
2021-01-19 20:48:22 +0700

    Add files via upload

--- commands/renungan.js
+++ commands/renungan.js
@@ -4,16 +4,16 @@ const { MessageType } = require("@adiwajshing/baileys")
 const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio, product } = MessageType
 
 exports.run = async (bot, message, args, from) => {
-    fetch(encodeURI(`https://twindev.herokuapp.com/api/v1/covid/indonesia`))
+    fetch(encodeURI(`https://docs-jojo.herokuapp.com/api/renungan`))
 			.then(response => response.json())
 			.then(data => {
-    bot.sendMessage(from,  `Covid19 In Indonesia\n\nPositif : ${data.positif} \nSembuh : ${data.sembuh} \nMeninggal : ${data.meninggal}`, text, { quoted: message });
+    bot.sendMessage(from,  `Renungan\n\nJudul : ${data.judul} \n\nRenungan : ${data.Isi} \n\nPesan : ${data.pesan}`, text, { quoted: message });
 });
 };
 
 exports.help = {
-    name: "Covid19",
-    description: "Show Data Covid19 in Indonesia",
-    usage: "covid19",
+    name: "renungan",
+    description: "Cerita untuk renungan",
+    usage: "renungan",
     cooldown: 5,
 };
